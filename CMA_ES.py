e0c32afb27c43b596a98276d8b6c67fbb42e47b8
Daniel <dzimmer@fzi.de>
2020-02-14 14:26:31 +0100

    Minor changes

--- CMA_ES.py
+++ CMA_ES.py
@@ -20,11 +20,11 @@ from scoop import futures
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size1, hidden_size2, output_size, individual, indirect_encoding=False):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.fc1 = nn.Linear(input_size, hidden_size1, bias=False)
         self.tanh1 = nn.Tanh()
-        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2, bias=False)
         self.tanh2 = nn.Tanh()
-        self.fc3 = nn.Linear(hidden_size2, output_size)
+        self.fc3 = nn.Linear(hidden_size2, output_size, bias=False)
         self.tanh3 = nn.Tanh()
 
         self.input_size = input_size
@@ -32,13 +32,14 @@ class NeuralNet(nn.Module):
         self.hidden_size2 = hidden_size2
         self.output_size = output_size
 
-        self.W1, self.W2, self.W3, self.B1, self.B2, self.B3 = self._set_weight_matrizes(individual, indirect_encoding)
+        self._set_weight_matrizes(individual, indirect_encoding)
+
         self.fc1.weight.data = torch.from_numpy(self.W1)
         self.fc2.weight.data = torch.from_numpy(self.W2)
         self.fc3.weight.data = torch.from_numpy(self.W3)
-        self.fc1.bias.data = torch.from_numpy(self.B1)
-        self.fc2.bias.data = torch.from_numpy(self.B2)
-        self.fc3.bias.data = torch.from_numpy(self.B3)
+        #self.fc1.bias.data = torch.from_numpy(self.B1)
+        #self.fc2.bias.data = torch.from_numpy(self.B2)
+        #self.fc3.bias.data = torch.from_numpy(self.B3)
 
     def forward(self, x):
         out = self.fc1(x)
@@ -61,49 +62,65 @@ class NeuralNet(nn.Module):
 
         W1_size = self.input_size*self.hidden_size1
         W2_size = self.hidden_size1*self.hidden_size2
-        W3_size = hidden_size2*output_size
+        W3_size = self.hidden_size2*self.output_size
 
         # Indirect encoding
         if indirect_encoding:
-            cppn = NeuralNet(4, cppn_hidden_size1, cppn_hidden_size2, 1, individual, indirect_encoding=False)
+            cppn_weights = NeuralNet(4, cppn_hidden_size1, cppn_hidden_size2, 1, individual[:cppn_weights_size], indirect_encoding=False)
 
-            W1 = np.zeros((self.hidden_size1, self.input_size), dtype=np.single)
-            W2 = np.zeros((self.hidden_size2, self.hidden_size1), dtype=np.single)
-            W3 = np.zeros((self.output_size, self.hidden_size2), dtype=np.single)
+            self.W1 = np.zeros((self.hidden_size1, self.input_size), dtype=np.single)
+            self.W2 = np.zeros((self.hidden_size2, self.hidden_size1), dtype=np.single)
+            self.W3 = np.zeros((self.output_size, self.hidden_size2), dtype=np.single)
 
-            for i, j in np.ndindex(W1.shape):
+            for i, j in np.ndindex(self.W1.shape):
                 temp = np.array([i/(self.hidden_size1-1),0.33,j/(self.input_size-1),0])
-                W1[i, j] = cppn.get_action(temp)
+                self.W1[i, j] = cppn_weights.get_action(temp)
 
-            for i, j in np.ndindex(W2.shape):
-                W2[i, j] = cppn.get_action(np.array([i/(self.hidden_size2-1),0.66,j/(self.hidden_size1-1),0.33]))
+            for i, j in np.ndindex(self.W2.shape):
+                self.W2[i, j] = cppn_weights.get_action(np.array([i/(self.hidden_size2-1),0.66,j/(self.hidden_size1-1),0.33]))
 
-            for i, j in np.ndindex(W3.shape):
-                W3[i, j] = cppn.get_action(np.array([i/(self.output_size-1),1.0,j/(self.hidden_size2-1),0.66]))
+            for i, j in np.ndindex(self.W3.shape):
+                self.W3[i, j] = cppn_weights.get_action(np.array([i/(self.output_size-1),1.0,j/(self.hidden_size2-1),0.66]))
+
+            # cppn_biases = NeuralNet(2, cppn_hidden_size1, cppn_hidden_size2, 1, individual[cppn_weights_size:], indirect_encoding=False)
+            #
+            # self.B1 = np.zeros(self.hidden_size1, dtype=np.single)
+            # self.B2 = np.zeros(self.hidden_size2, dtype=np.single)
+            # self.B3 = np.zeros(self.output_size, dtype=np.single)
+            #
+            # for i in range(self.hidden_size1):
+            #     self.B1[i] = cppn_biases.get_action(np.array([i / (self.hidden_size1 - 1), 0.33]))
+            #
+            # for i in range(self.hidden_size2):
+            #     self.B2[i] = cppn_biases.get_action(np.array([i / (self.hidden_size2 - 1), 0.66]))
+            #
+            # for i in range(self.output_size):
+            #     self.B3[i] = cppn_biases.get_action(np.array([i / (self.output_size - 1), 1.0]))
 
         # Direct encoding
         else:
             # Weight Matrizes
-            W1 = np.array([[float(element)] for element in individual[0:W1_size]], dtype=np.single)
-            W2 = np.array([[float(element)] for element in individual[W1_size:W1_size+W2_size]], dtype=np.single)
-            W3 = np.array([[float(element)] for element in individual[W1_size+W2_size:W1_size+W2_size+W3_size]], dtype=np.single)
+            self.W1 = np.array([[float(element)] for element in individual[0:W1_size]], dtype=np.single)
+            self.W2 = np.array([[float(element)] for element in individual[W1_size:W1_size+W2_size]], dtype=np.single)
+            self.W3 = np.array([[float(element)] for element in individual[W1_size+W2_size:W1_size+W2_size+W3_size]], dtype=np.single)
 
             # Bias Matrizes
             index_b = W1_size + W2_size + W3_size
-            B1 = np.array([float(element) for element in individual[index_b:index_b+hidden_size1]], dtype=np.single)
-            B2 = np.array([float(element) for element in individual[index_b+hidden_size1:index_b+hidden_size1+hidden_size2]], dtype=np.single)
-            B3 = np.array([float(element) for element in individual[index_b+hidden_size1+hidden_size2:]], dtype=np.single)
+            self.B1 = np.array([float(element) for element in individual[index_b:index_b+self.hidden_size1]], dtype=np.single)
+            self.B2 = np.array([float(element) for element in individual[index_b+self.hidden_size1:index_b+self.hidden_size1+self.hidden_size2]], dtype=np.single)
+            self.B3 = np.array([float(element) for element in individual[index_b+self.hidden_size1+self.hidden_size2:]], dtype=np.single)
 
-            W1 = W1.reshape([self.hidden_size1, self.input_size])
-            W2 = W2.reshape([self.hidden_size2, self.hidden_size1])
-            W3 = W3.reshape([self.output_size, self.hidden_size2])
+            self.W1 = self.W1.reshape([self.hidden_size1, self.input_size])
+            self.W2 = self.W2.reshape([self.hidden_size2, self.hidden_size1])
+            self.W3 = self.W3.reshape([self.output_size, self.hidden_size2])
 
-        # Normalize
-        # W1 = (W1 - W1.mean()) / max(W1.std(), 0.1)
-        # W2 = (W2 - W2.mean()) / max(W2.std(), 0.1)
-        # W3 = (W3 - W3.mean()) / max(W3.std(), 0.1)
-
-        return W1, W2, W3, B1, B2, B3
+            # Normalize
+            #self.W1 = (self.W1 - self.W1.mean()) / max(self.W1.std(), 0.1)
+            #self.W2 = (self.W2 - self.W2.mean()) / max(self.W2.std(), 0.1)
+            #self.W3 = (self.W3 - self.W3.mean()) / max(self.W3.std(), 0.1)
+            #self.B1 = (self.B1 - self.B1.mean()) / max(self.B1.std(), 0.1)
+            #self.B2 = (self.B2 - self.B2.mean()) / max(self.B2.std(), 0.1)
+            #self.B3 = (self.B3 - self.B3.mean()) / max(self.B3.std(), 0.1)
 
     def get_weight_matrizes(self):
         return self.W1, self.W2, self.W3
@@ -111,7 +128,7 @@ class NeuralNet(nn.Module):
 
 def evalFitness(individual):
 
-    model = NeuralNet(input_size, hidden_size1, hidden_size2, output_size, individual, indirect_encoding=False)
+    model = NeuralNet(input_size, hidden_size1, hidden_size2, output_size, individual, indirect_encoding=True)
 
     fitness_current = 0
     ob = env.reset()
@@ -143,12 +160,15 @@ hidden_size1 = 32
 hidden_size2 = 8
 output_size = env.action_space.shape[0]
 
-cppn_hidden_size1 = 12
-cppn_hidden_size2 = 6
+cppn_hidden_size1 = 8
+cppn_hidden_size2 = 4
 
 # Size of Individual
-IND_SIZE=input_size*hidden_size1+hidden_size1*hidden_size2+hidden_size2*output_size + hidden_size1 + hidden_size2 + output_size
-#IND_SIZE=4*cppn_hidden_size1+cppn_hidden_size1*cppn_hidden_size2+cppn_hidden_size2*1
+#IND_SIZE=input_size*hidden_size1+hidden_size1*hidden_size2+hidden_size2*output_size + hidden_size1 + hidden_size2 + output_size
+
+cppn_weights_size = 4*cppn_hidden_size1+cppn_hidden_size1*cppn_hidden_size2+cppn_hidden_size2*1 + cppn_hidden_size1 + cppn_hidden_size2 + 1
+cppn_biases_size = 2*cppn_hidden_size1+cppn_hidden_size1*cppn_hidden_size2+cppn_hidden_size2*1 + cppn_hidden_size1 + cppn_hidden_size2 + 1
+IND_SIZE= cppn_weights_size# + cppn_biases_size
 
 print(IND_SIZE)
 
@@ -161,8 +181,8 @@ toolbox = base.Toolbox()
 toolbox.register("map", futures.map)
 toolbox.register("evaluate", evalFitness)
 
-# strategy = cma.Strategy(centroid=[0.0] * IND_SIZE, sigma=1.0, lambda_= 200)
-strategy = cma.Strategy(centroid=[0.0] * IND_SIZE, sigma=5.0)
+strategy = cma.Strategy(centroid=[0.0] * IND_SIZE, sigma=1.0, lambda_= 200)
+# strategy = cma.Strategy(centroid=[0.0] * IND_SIZE, sigma=5.0)
 toolbox.register("generate", strategy.generate, creator.Individual)
 toolbox.register("update", strategy.update)
 
@@ -183,7 +203,7 @@ if __name__ == "__main__":
 
     best_individual = hof[0]
 
-    best_model = NeuralNet(input_size, hidden_size1, hidden_size2, output_size, best_individual, indirect_encoding=False)
+    best_model = NeuralNet(input_size, hidden_size1, hidden_size2, output_size, best_individual, indirect_encoding=True)
     W1, W2, W3 = best_model.get_weight_matrizes()
 
     # Save weights of hof individual
