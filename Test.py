6cd916cdcde9a0a5cabef2ee065820535c544e58
Daniel <dzimmer@fzi.de>
2020-02-19 12:28:22 +0100

    json loading added

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ import mujoco_py
 import gym
 import time
 import numpy as np
+import json
 
 env = gym.make('InvertedPendulum-v2')
 # env = gym.make('BipedalWalker-v3')
@@ -14,9 +15,19 @@ temp2 = env.action_space.shape[0]
 print(temp1)
 print(temp2)
 
+with open("Configuration_Optimization.json", "r") as read_file:
+    data = json.load(read_file)
+
+print(data["environment"])
+print(data["number_neurons"])
+print(data["delta_t"])
+print(data["population_size"])
+print(data["number_generations"])
+print(data["number_fitness_runs"])
 
 done = False
 fitness = 0
+
 while not done:
     temp = env.action_space.sample()
     ob, rew, done, info = env.step(env.action_space.sample()) # take a random action
