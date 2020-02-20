f4a19a5b44bd598e05c1d1d0c03edbb0fc548bc7
Daniel <dzimmer@fzi.de>
2020-02-20 11:14:22 +0100

    Optimization of y0 and random seed for gym environment added

--- CTRNN_ReinforcementLearning_CMA-ES.py
+++ CTRNN_ReinforcementLearning_CMA-ES.py
@@ -31,27 +31,33 @@ def evalFitness(individual):
 
     V_size = input_size * number_neurons
     W_size = number_neurons * number_neurons
+    T_size = number_neurons * output_size
 
     # Get weight matrizes of current individual
     V = np.array([[element] for element in individual[0:V_size]])
     W = np.array([[element] for element in individual[V_size:V_size+W_size]])
-    T = np.array([[element] for element in individual[V_size+W_size:]])
+    T = np.array([[element] for element in individual[V_size+W_size:V_size+W_size+T_size]])
 
     V = V.reshape([number_neurons, input_size])
     W = W.reshape([number_neurons, number_neurons])
     T = T.reshape([number_neurons, output_size])
 
     # Set elements of main diagonal to less than 0
-    for j in range(number_neurons):
-        W[j][j] = -abs(W[j][j])
+    if configuration_data["set_priciple_diagonal_elements_of_W_negative"]:
+        for j in range(number_neurons):
+            W[j][j] = -abs(W[j][j])
 
     fitness_current = 0
+    number_fitness_runs = configuration_data["number_fitness_runs"]
 
+    for i in range(number_fitness_runs):
 
-    for i in range(configuration_data["number_fitness_runs"]):
+        # Anfangswerte für y
+        if configuration_data["optimize_y0"]:
+            y = np.array([element for element in individual[V_size+W_size+T_size:]])
+        else:
+            y = np.zeros(number_neurons)
 
-        # Anfangswerte
-        y = np.zeros(number_neurons)
         y = y[:, np.newaxis]
 
         ob = env.reset()
@@ -73,7 +79,7 @@ def evalFitness(individual):
 
             fitness_current += rew
 
-    return fitness_current/configuration_data["number_fitness_runs"],
+    return fitness_current/number_fitness_runs,
 
 
 # Load configuration file
@@ -81,6 +87,10 @@ with open("Configuration.json", "r") as read_file:
     configuration_data = json.load(read_file)
 
 env = gym.make(configuration_data["environment"])
+
+# Set random seed for gym environment
+if isinstance(configuration_data["random_seed_for_environment"], int):
+    env.seed(configuration_data["random_seed_for_environment"])
 
 # Number of neurons
 number_neurons = configuration_data["number_neurons"]
@@ -94,6 +104,9 @@ delta_t = configuration_data["delta_t"]
 
 # Size of Individual
 IND_SIZE = input_size * number_neurons + number_neurons * number_neurons + number_neurons * output_size
+
+if configuration_data["optimize_y0"]:
+    IND_SIZE += number_neurons
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, typecode='b', fitness=creator.FitnessMax)
