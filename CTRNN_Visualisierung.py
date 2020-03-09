1297c55e86fdb715fac7bf06f2085cedeb16ad2f
Daniel <dzimmer@fzi.de>
2020-03-09 11:14:31 +0100

    Adapted for Config File

--- CTRNN_Visualisierung.py
+++ CTRNN_Visualisierung.py
@@ -4,8 +4,10 @@ import pickle
 import os
 import json
 import time
-import brains.continuous_time_rnn as ctrnn
 import matplotlib.pyplot as plt
+import brains.continuous_time_rnn as ctrnn
+import brains.layered_nn as lnn
+import sys
 
 from deap import base
 from deap import creator
@@ -13,18 +15,18 @@ from deap import creator
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, typecode='b', fitness=creator.FitnessMax)
 
-directory = '2020-02-23_18-34-48'
+directory = '2020-03-05_07-51-21'
 
 # Load configuration file
-with open(os.path.join('Simulation_Results', 'CTRNN', directory, 'Configuration.json'), "r") as read_file:
+with open(os.path.join('Simulation_Results', 'LNN', directory, 'Configuration.json'), "r") as read_file:
     configuration_data = json.load(read_file)
 
 # Load hall of fame candidates
-with open(os.path.join('Simulation_Results', 'CTRNN', directory, 'HallOfFame.pickle'), "rb") as read_file:
+with open(os.path.join('Simulation_Results', 'LNN', directory, 'HallOfFame.pickle'), "rb") as read_file:
     hall_of_fame = pickle.load(read_file)
 
 # Load Log
-with open(os.path.join('Simulation_Results', 'CTRNN', directory, 'Log.json'), 'r') as read_file:
+with open(os.path.join('Simulation_Results', 'LNN', directory, 'Log.json'), 'r') as read_file:
     log = json.load(read_file)
 
 # Get individual from hall of fame
@@ -36,26 +38,29 @@ env = gym.make(configuration_data["environment"])
 if configuration_data["random_seed_for_environment"] is not -1:
     env.seed(configuration_data["random_seed_for_environment"])
 
+# Get individual size
+input_size = env.observation_space.shape[0]
+output_size = env.action_space.shape[0]
+
+# Get brain class
+if configuration_data["neural_network_type"] == 'LNN':
+    brain_class = lnn.LayeredNN
+elif configuration_data["neural_network_type"] == 'CTRNN':
+    brain_class = ctrnn.ContinuousTimeRNN
+else:
+    sys.exit()
+
 env.render()
 env.reset()
 
-for i in range(1000):
+for i in range(1):
 
     fitness_current = 0
     ob = env.reset()
     done = False
 
-    # Create Contininuous-Time RNN object
-    brain = ctrnn.ContinuousTimeRNN(individual,
-                                    env.observation_space.shape[0],
-                                    configuration_data["number_neurons"],
-                                    env.action_space.shape[0],
-                                    configuration_data["optimize_y0"],
-                                    configuration_data["delta_t"],
-                                    configuration_data["optimize_state_boundaries"],
-                                    configuration_data["clipping_range_min"],
-                                    configuration_data["clipping_range_max"],
-                                    configuration_data["set_principle_diagonal_elements_of_W_negative"])
+    # Create brain
+    brain = brain_class(input_size, output_size, individual, configuration_data)
 
     # Test fitness through simulation
     while not done:
